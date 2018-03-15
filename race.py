de4b43890db3a013ff701a652d44da56ad96c35b
Rohit Saluja <salujarohit@outlook.com>
2018-03-15 07:39:38 +0530

    obstacle

--- race.py
+++ race.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+import random 
 
 pygame.init() #initiating pygame modules
 
@@ -8,21 +9,29 @@ display_height = 600
 
 black = (0,0,0) 
 white = (255,255,255)
+red = (255,0,0)
 
 ship_width = 99
 
 
 gameDisplay = pygame.display.set_mode((display_width,display_height)) #Initialize window or screen to run the game
-pygame.display.set_caption('Race')
+pygame.display.set_caption('Space Race')
 clock = pygame.time.Clock() #setting game clock to track time inside game
 
 shipImg = pygame.image.load('ship.png')
+
+
+def obstacles(obsx , obsy , obswidth , obsheight , obscolor):
+	pygame.draw.rect(gameDisplay, obscolor , [obsx, obsy, obswidth, obsheight])
+
+
+
 
 def ship(x,y):
 	gameDisplay.blit(shipImg,(x,y))
 
 def text_objects(text , font):
-	textSurface = font.render(text , True , black)
+	textSurface = font.render(text , True , black )
 	return(textSurface , textSurface.get_rect()) 
 
 
@@ -35,13 +44,13 @@ def message_display(text):
 	pygame.display.update()
 
 	time.sleep(2)
- 
 	
 
 
 
 def crash():
 	message_display('You Crashed')
+	
 	game_loop()
 
 
@@ -51,6 +60,12 @@ def game_loop():
 	y = (display_height * 0.8)
 
 	x_change = 0
+
+	obs_startx = random.randrange(2 , display_width -102 )
+	obs_starty = -600
+	obs_speed = 7
+	obs_width = 100
+	obs_height = 100
 
 	gameExit = False
 
@@ -90,10 +105,39 @@ def game_loop():
 		
 			
 		gameDisplay.fill(white)
+
+
+
+
+		obstacles(obs_startx, obs_starty, obs_width, obs_height, black)
+		obs_starty += obs_speed
+
+
+
+
+
+
+
+
 		ship(x,y)
 
 		if x > display_width - ship_width  or x < 0:
 			crash() 
+
+		if obs_starty > display_height:
+			obs_starty = -100
+			obs_startx = random.randrange(2 , display_width -102 )
+
+
+		if y < obs_starty + obs_height:
+			#print("y crossover")
+
+			if x > obs_startx and x < obs_startx + obs_width or x + ship_width > obs_startx and x + ship_width < obs_startx + obs_width:
+				crash()
+
+
+
+
 		
 		pygame.display.update()
 		clock.tick(60) #updating the window at 60 fps
